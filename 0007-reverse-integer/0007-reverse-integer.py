4b9a7ab9cf846d63ac947303d21802354d773b0f
Rajib Kumar Maharana <152739528+rajibmaharana8@users.noreply.github.com>
2025-12-04 00:51:17 +0530

    Time: 18 ms (54.21%), Space: 12.3 MB (98.39%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        rev = 0
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+
+        while x != 0:
+            pop = x % 10
+            x //= 10
+
+            # Check overflow before multiplying
+            if rev > INT_MAX // 10 or (rev == INT_MAX // 10 and pop > 7):
+                return 0
+            rev = rev * 10 + pop
+
+        return sign * rev
