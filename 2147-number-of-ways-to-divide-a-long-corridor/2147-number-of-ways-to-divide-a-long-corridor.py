33753cbeaca50cbd58ddb60e9288434e82f92a39
Rajib Kumar Maharana <152739528+rajibmaharana8@users.noreply.github.com>
2025-12-14 23:57:29 +0530

    Time: 412 ms (45.45%), Space: 13.1 MB (63.64%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def numberOfWays(self, corridor):
-        """
-        :type corridor: str
-        :rtype: int
-        """
         MOD = 10**9 + 7
         seats = corridor.count('S')
-        
-        # If no seats or odd number of seats, impossible
+
         if seats == 0 or seats % 2 == 1:
             return 0
         
@@ -20,15 +15,13 @@ class Solution(object):
             if ch == 'S':
                 seat_count += 1
                 if seat_count % 2 == 0:
-                    # Completed a pair, start counting plants until next seat
                     counting_plants = True
                 else:
-                    # First seat of next pair → finalize plant block
                     if counting_plants:
                         ways = (ways * (plants_between_pairs + 1)) % MOD
                         plants_between_pairs = 0
                         counting_plants = True
-            else:  # ch == 'P'
+            else:
                 if counting_plants and seat_count % 2 == 0:
                     plants_between_pairs += 1
         
