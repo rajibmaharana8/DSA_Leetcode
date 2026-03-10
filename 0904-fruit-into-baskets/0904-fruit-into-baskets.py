59d453bf65674d439128a408839551539156ac20
Rajib Kumar Maharana <152739528+rajibmaharana8@users.noreply.github.com>
2026-03-10 20:33:49 +0530

    Time: 171 ms (82.91%), Space: 26.1 MB (11.96%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        max_len , right,left = 0,0,0
+        my_dict={}
+
+        while right<len(fruits):
+            my_dict[fruits[right]] = my_dict.get(fruits[right],0)+1
+            if len(my_dict)>2:
+                my_dict[fruits[left]] -= 1
+                if(my_dict[fruits[left]]==0):
+                    del my_dict[fruits[left]]
+
+                left+=1
+            if len(my_dict)<=2:
+                max_len = max(max_len,right-left+1)
+            
+            right+=1
+            
+        return max_len
