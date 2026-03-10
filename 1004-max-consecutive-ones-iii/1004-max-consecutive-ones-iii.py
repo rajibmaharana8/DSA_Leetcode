50ff649f1d1d95ce6e4d72bb6474a021ac36a620
Rajib Kumar Maharana <152739528+rajibmaharana8@users.noreply.github.com>
2026-03-10 20:09:26 +0530

    Time: 72 ms (18.94%), Space: 22.4 MB (22.74%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -9,7 +9,8 @@ class Solution:
                 if nums[left]==0:
                     zeroes-=1
                 left+=1
-            maxx = max(maxx,right-left+1)
+            if zeroes<=k:
+                maxx = max(maxx,right-left+1)
             right+=1
         return maxx
 
