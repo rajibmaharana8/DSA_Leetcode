4bd82506ab823977699fff0a7fe19196fe611ba8
Rajib Kumar Maharana <152739528+rajibmaharana8@users.noreply.github.com>
2026-05-10 18:21:41 +0530

    Time: 427 ms (32.28%), Space: 45.1 MB (30.6%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -5,12 +5,12 @@ class Solution:
         m = len(text2)
         maxi =0
 
-        dp = [[-1 for _ in range(m+1)]for _ in range(n+1)]
+        dp = [[0 for _ in range(m+1)]for _ in range(n+1)]
 
-        for i in range(0,n+1):
+        """for i in range(0,n+1):
             dp[i][0] = 0
         for j in range(0,m+1):
-            dp[0][j] = 0
+            dp[0][j] = 0"""
 
         for i in range(1,n+1):
             for j in range(1,m+1):
